cecb700adcb24ac2a68b351d5cf7e5c951a20ad9
Arne Bedurftig <dasnervtdoch@googlemail.com>
2017-12-17 01:01:29 +0100

    Ready to query the server!

--- poi.config.js
+++ poi.config.js
@@ -1,3 +1,14 @@
-module.exports = (options, req) => ({
-    entry: './src/index.js' 
-})
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    webpack(config) {
+        config.plugins.push(new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+                API_URL: JSON.stringify(process.env.API_URL),
+            },
+        }));
+        return config; // <-- Important, must return it
+    }
+}
--- src/app/App.js
+++ src/app/App.js
@@ -1,2 +1,44 @@
-const App = ({ message }) => <div>{message}</div>
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            projects:[],
+            user: {
+                firstName: "Max",
+                lastName: "Admin",
+                accountId: 1
+            }
+        };
+    }
+
+    componentDidMount() {
+        console.log(process.env);
+        fetch("http://localhost:8080/api/account/1/projects").then(result => {
+            return result.json();
+        }).then(projects => {
+
+            let projectsEl = projects.map(pr => {
+                return <li key={pr.id}>{pr.name}</li>
+            })    
+
+            this.setState({projects: projectsEl});
+        }).catch(function(error) {
+            console.log('There has been a problem with your fetch operation: ', error.message);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.message}
+                <ul>
+                    {this.state.projects}
+                </ul>
+            </div>
+        )
+    }
+
+}    
+
 export default App;
