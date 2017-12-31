8e45d70f225a4690bf64b9b861aced32eff4184a
arnebeduerftig <arne.beduerftig@friday.de>
2017-12-31 18:02:17 +0100

    Updated engines and UI improvements

--- src/app/app.js
+++ src/app/app.js
@@ -5,16 +5,20 @@ import Projects from './containers/projects'
 import Project from './containers/projects/project'
 
 class App extends React.Component {
-
   render() {
     return (
       <div>
         <main>
           <Route exact path="/" render={() => <Redirect to="/projects"/>} />
-          {/* <Route exact path="/about-us" component={About} /> */}
+          <Route exact path="/about-us" component={About} />
           <Route exact path="/projects" component={Projects} />
           <Route path="/projects/:id" component={Project} />
         </main>
+        <footer className="footer">
+          <div className="container">
+            <Link to="/about-us">About Us</Link>
+          </div>
+        </footer>
       </div>
     )
   }
