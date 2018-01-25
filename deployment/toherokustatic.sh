b40f039716fb5b41ee675abde2b5992bdca288d9
Arne Bedurftig <dasnervtdoch@googlemail.com>
2018-01-25 20:23:37 +0100

    Deployment from Travis to Heroku.

--- deployment/toherokustatic.sh
+++ deployment/toherokustatic.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-cd $CI_PROJECT_DIR/heroku
+cd $TRAVIS_BUILD_DIR/heroku
 git init
 git config user.name $HEROKU_GIT_USER_NAME
 git config user.email $HEROKU_GIT_USER_EMAIL
-#heroku git:remote $HEROKU_APP_NAME
-heroku git:remote tdb-app
-git add -A && git commit -m "Test"
-#git push https://heroku:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git master -f
-git push https://heroku:$HEROKU_API_KEY@git.heroku.com/tdb-app.git master -f
+heroku git:remote $HEROKU_APP_NAME
+git add -A && git commit -m $TRAVIS_BRANCH@$TRAVIS_BUILD_NUMBER
+git push https://heroku:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git master -f
