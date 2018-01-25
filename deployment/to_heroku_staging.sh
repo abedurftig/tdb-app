7b96472d55d989bcd136caa18391ae6863835e1a
Arne Bedurftig <dasnervtdoch@googlemail.com>
2018-01-25 22:45:03 +0100

    Deployment to staging.

---
+++ deployment/to_heroku_staging.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $TRAVIS_BUILD_DIR/heroku
+git init
+git config user.name $HEROKU_GIT_USER_NAME
+git config user.email $HEROKU_GIT_USER_EMAIL
+heroku git:remote $HEROKU_APP_NAME-staging
+git add -A && git commit -m $TRAVIS_BRANCH@$TRAVIS_BUILD_NUMBER
+git push https://heroku:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME-staging.git master -f
