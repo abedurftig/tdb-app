1904ea1617c740e933b91267afd233172c277dca
Arne Bedurftig <dasnervtdoch@googlemail.com>
2018-02-28 23:55:47 +0100

    Fixed Docker build.

--- docker/build-push-docker.sh
+++ docker/build-push-docker.sh
@@ -11,7 +11,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] || [[ $TRAVIS_BRANCH =~ v[0-9]+\.[0-9]+\.[0-
         tag=${branch_name//v/""};
     fi
 
-    docker build -f build/docker/jdk8/Dockerfile -t $repo:$tag .
+    docker build -f docker/Dockerfile -t $repo:$tag .
     echo "Pushing tag '$tag' to Docker ($repo)."
     docker push $repo
 
